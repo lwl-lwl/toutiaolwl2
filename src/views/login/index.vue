3552d9334dd4d708543e9a03df6b9d83fb61c67d
lwl_lwl <979510482@qq.com>
2019-10-26 11:31:40 +0800

    登录模块-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="container">login</div>
+  <div class="container">
+    <el-card>
+      <img src="../../assets/logo_index.png" alt />
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -7,4 +11,26 @@ export default {}
 </script>
 
 <style scoped lang='less'>
+.container {
+  width: 100%;
+  height: 100%;
+  background: pink;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+  .el-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto;
+    }
+  }
+}
 </style>
