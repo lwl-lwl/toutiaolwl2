3c638cb3a077f34513673b7a56247a5bb29cf485
lwl_lwl <979510482@qq.com>
2019-10-26 11:34:06 +0800

    登录模块-绘制表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,12 +2,40 @@
   <div class="container">
     <el-card>
       <img src="../../assets/logo_index.png" alt />
+      <el-form :model="LoginForm">
+        <el-form-item>
+          <el-input v-model="LoginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="LoginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;margin-right:8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" style="width:100%;">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      LoginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
@@ -29,7 +57,7 @@ export default {}
     img {
       width: 200px;
       display: block;
-      margin: 0 auto;
+      margin: 0 auto 30px;
     }
   }
 }
