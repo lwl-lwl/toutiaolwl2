831130a453c9021ee5dd1971335585b96970c69c
lwl_lwl <979510482@qq.com>
2019-10-31 21:49:40 +0800

    内容管理-列表渲染-分页功能

--- src/views/article/article.vue
+++ src/views/article/article.vue
@@ -44,21 +44,50 @@
     <!-- 筛选结果布局 -->
     <el-card style="margin-top:20px">
       <div slot="header">
-        <span>根据筛选条件共查询到 0 条结果：</span>
+        <span>根据筛选条件共查询到 {{total}} 条结果：</span>
       </div>
       <!-- 表格 -->
-      <el-table>
-        <el-table-column label="封面"></el-table-column>
-        <el-table-column label="标题"></el-table-column>
-        <el-table-column label="状态"></el-table-column>
-        <el-table-column label="发布时间"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+      <el-table :data="articles">
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+            <el-image :src="scope.row.cover.images[0]" style="width:150px;height:100px">
+              <div slot="error">
+                <img src="../../assets/error.gif" alt width="150" height="100" />
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status===0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status===1" type="success">待审核</el-tag>
+            <el-tag v-if="scope.row.status===2" type="warning">审核通过</el-tag>
+            <el-tag v-if="scope.row.status===3" type="danger">审核失败</el-tag>
+            <el-tag v-if="scope.row.status===4" type="pubdate">已删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" circle plain></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle plain></el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
-      <el-pagination style="margin-top:20px" background layout="prev,pager,next" :total="1000"></el-pagination>
+      <el-pagination
+        style="margin-top:20px"
+        background
+        layout="prev,pager,next"
+        :total="total"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="pager"
+      ></el-pagination>
     </el-card>
   </div>
-</template>
+</template>+
 
 <script>
 export default {
@@ -68,14 +97,23 @@ export default {
         status: null,
         channel_id: null,
         begin_pubdate: null,
-        end_pubdate: null
+        end_pubdate: null,
+        // 当前页码
+        page: 1,
+        // 每页条数
+        per_page: 10
       },
       channelOptions: [],
-      dateArr: []
+      dateArr: [],
+      articles: [],
+      // 总条数
+      total: 0
     }
   },
   created () {
     this.getchannelOptions()
+    this.getArticles()
+    // this.getArticles()
   },
   methods: {
     async getchannelOptions () {
@@ -85,6 +123,22 @@ export default {
       } = await this.$http.get('channels')
       // 赋值
       this.channelOptions = data.channels
+    },
+    // 获取文章列表数据
+    async getArticles () {
+      // 获取数据
+      const {
+        data: { data }
+      } = await this.$http.get('articles', { params: this.reqParams })
+      // 赋值 articles
+      this.articles = data.results
+      // 赋值总条数
+      this.total = data.total_count
+    },
+    pager (newPage) {
+      // 修改当前页码为新的页码
+      this.reqParams.page = newPage
+      this.getArticles()
     }
   }
 }
